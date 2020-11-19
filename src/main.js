7b0e17426cda4491781ae148a91664a8e86fa984
HarrisonStrand <harrisonstrand@gmail.com>
2020-11-19 10:03:23 -0800

    add branching to displayWeather function to display sol AT temperature
    Co-authored-by: Eric Stratton <strattonericj@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,14 +5,15 @@ import './css/styles.css';
 import Weather from './services/Elysium-Planitia.js';
 
 //Business Logic
-function makeWeatherCall(date) {
-  const response = Weather.getWeather(date);
-}
 
 function displayWeather(response) {
-  for(const property in response) {
-    $('#dayCurrent').text(`Today's Martian weather is ${response[property].AT.mn}`);
-    $('#dayCurrent').text(`Today's Martian weather is ${response[property].AT.mx}`);
+  for(let property in response) {
+    if (response[property].AT) {
+      $('#dayCurrent').append(`${response[property].AT.mn}, `);
+    }
+      
+    // $('#dayCurrent').text(`Today's Martian weather is ${response[698].Season}`);
+    // $('#dayCurrent').text(`Today's Martian weather is ${response[property].AT.mx}`);
   }
 }
 
@@ -24,6 +25,10 @@ function displayWeather(response) {
 //User Interface Logic
 
 $(document).ready(function() {
-  displayWeather(response);
-
+  $("#button").click(function() {
+    Weather.getWeather()
+      .then(function(response) {
+        displayWeather(response);
+      });
+  });
 });
--- src/services/Elysium-Planitia.js
+++ src/services/Elysium-Planitia.js
@@ -1,5 +1,5 @@
 export default class Weather {
-  static async getWeather() {
+  static getWeather() {
     return fetch(`https://api.nasa.gov/insight_weather/?api_key=${process.env.API_KEY}&feedtype=json&ver=1.0`)
       .then(function(response) {
         if (!response.ok) {
@@ -9,6 +9,6 @@ export default class Weather {
       })
       .catch(function(error) {
         return Error(error);
-      })
+      });
   }
 }
